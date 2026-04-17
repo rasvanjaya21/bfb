96f161f77c90d2df0e91e7887d1cc90dd0702e98
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-17 18:06:38 +0700

    feat(util): billy missing

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,4 @@
-const VERSION: string = 'v0.1.0';
+const VERSION: string = 'v0.1.2';
 const MOTIVATIONS: string[] = ['ngacwng titik lali dunyo', 'budal moleh gak sempaksn', 'ndasku mumet ndasmu piye', 'turuo indonesia opo jare aku', 'mantun mantuk manwk'];
 const DRIVER_VERSION: string = '147.0.7727.101';
 
