70ca1cb6c3662335a9a1353ed3c82e18f2dc051b
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-20 12:48:31 +0700

    feat(ignore):standardize publish enable checker

--- src/ignore/index.ts
+++ src/ignore/index.ts
@@ -1,17 +1,27 @@
 import { applyDelay } from '@/libs/apply-delay';
-import type { ElementHandle } from 'puppeteer-core';
+import type { Page } from 'puppeteer-core';
 
-async function isFacebookPublishButtonEnable(button: ElementHandle<Element> | null): Promise<boolean> {
-	while (true) {
-		const isDisabled = await button?.evaluate((element) => {
-			const attr = element.getAttribute('aria-disabled');
-			return attr === 'true';
-		});
+async function isFacebookPublishButtonEnable(page: Page, selector: string): Promise<boolean> {
+	let isDisabled = true;
 
-		if (!isDisabled) break;
+	while (isDisabled) {
+		isDisabled = await page.evaluate((selector) => {
+			const xpath = selector.replace('xpath=', '');
+			const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+			const node = result.singleNodeValue;
 
-		await applyDelay(1000);
+			if (!node || !(node instanceof Element)) {
+				return true;
+			}
+
+			return node.getAttribute('aria-disabled') === 'true';
+		}, selector);
+
+		if (isDisabled) {
+			await applyDelay(1000);
+		}
 	}
+	await applyDelay(1000);
 	return true;
 }
 
