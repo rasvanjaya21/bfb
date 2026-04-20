4d9a75e4e34c71dcde1bfc05e99b85d9c23452b9
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-20 12:43:13 +0700

    feat(util): update motivation n project minor version

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
-const VERSION: string = 'v0.1.12';
-const MOTIVATIONS: string[] = ['ngacwng titik lali dunyo', 'budal moleh gak sempaksn', 'ndasku mumet ndasmu piye', 'turuo indonesia opo jare aku', 'mantun mantuk manwk'];
+const VERSION: string = 'v0.2.0';
+const MOTIVATIONS: string[] = ['ngacwng titik lali dunyo', 'budal moleh gak sempaksn', 'ndasku mumet ndasmu piye', 'turuo indonesia opo jare aku', 'mantun mantuk manwk', 'madep mantep mancep'];
 const DRIVER_VERSION: string = '147.0.7727.101';
 
 export { DRIVER_VERSION, MOTIVATIONS, VERSION };
