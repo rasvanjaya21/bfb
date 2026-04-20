ab433e526c9d5463b342e3b7f70e2c51e4de6022
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-20 12:47:05 +0700

    feat(lib): add point two format duration

--- src/libs/format-duration.ts
+++ src/libs/format-duration.ts
@@ -1,10 +1,15 @@
 function formatDuration(ms: number) {
 	const totalSeconds = Math.floor(ms / 1000);
+
 	const h = Math.floor(totalSeconds / 3600);
 	const m = Math.floor((totalSeconds % 3600) / 60);
 	const s = totalSeconds % 60;
 
-	return `${h}j ${m}m ${s}d`;
+	const hh = String(h).padStart(2, '0');
+	const mm = String(m).padStart(2, '0');
+	const ss = String(s).padStart(2, '0');
+
+	return `${hh}j ${mm}m ${ss}d`;
 }
 
 export { formatDuration };
