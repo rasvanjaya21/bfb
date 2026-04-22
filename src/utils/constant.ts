f9795c88ce6af8617ead678619fa0b7a2ea99654
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-22 12:17:53 +0700

    feat(constant): update patch version

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,4 @@
-const VERSION: string = 'v0.3.0';
+const VERSION: string = 'v0.3.1';
 const MOTIVATIONS: string[] = ['ngacwng titik lali dunyo', 'budal moleh gak sempaksn', 'ndasku mumet ndasmu piye', 'turuo indonesia opo jare aku', 'mantun mantuk manwk', 'madep mantep mancep'];
 const DRIVER_VERSION: string = '147.0.7727.101';
 
