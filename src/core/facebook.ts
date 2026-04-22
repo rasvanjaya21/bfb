6407e0e038037f73637a00980d77d7fa061cec16
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-04-22 12:09:54 +0700

    feat(core): match new flow, some logs change

--- src/core/facebook.ts
+++ src/core/facebook.ts
@@ -46,6 +46,24 @@ async function postFeed(browser: Browser, content: Content): Promise<void> {
 	console.log('===============================');
 	console.log(`Data konten nomor ${content.NO}`);
 
+	let link = '';
+	let fileSelector = '';
+	let captionSelector = '';
+
+	console.log('Mengecek rute upload');
+	if (content.ROUTE === '') {
+		console.log('Rute upload kosong');
+		return;
+	}
+	if (content.ROUTE === 'BM') {
+		console.log(`Rute upload ${content.ROUTE.toLowerCase()}`);
+		link = `https://business.facebook.com/latest/composer/?asset_id=${content.IDFANSPAGE}&ref=biz_web_home_create_post`;
+	}
+	if (content.ROUTE === 'PERSONAL') {
+		console.log(`Rute upload ${content.ROUTE.toLowerCase()}`);
+		link = 'https://web.facebook.com/login.php?next=https://web.facebook.com/profile/';
+	}
+
 	const page = await browser.newPage();
 	const pages = await browser.pages();
 	const initialPage = pages[0];
@@ -56,18 +74,18 @@ async function postFeed(browser: Browser, content: Content): Promise<void> {
 	const cookies = await readCookies<CookieData>(cookiesPath, content.COOKIE);
 	await browser.setCookie(...cookies);
 
-	console.log('Membuka meta business');
-	const pageOpener = await page.goto(`https://business.facebook.com/latest/composer/?asset_id=${content.IDFANSPAGE}&ref=biz_web_home_create_post`, { waitUntil: 'domcontentloaded' }).catch(() => null);
+	console.log('Membuka facebook');
+	const pageOpener = await page.goto(link, { waitUntil: 'networkidle2' }).catch(() => null);
 
 	if (!pageOpener) {
-		console.log('Meta business tidak terbuka');
+		console.log('Facebook tidak terbuka');
 		await browser.deleteMatchingCookies(...cookies);
 		return;
 	}
-	console.log('Meta business terbuka');
+	console.log('Facebook terbuka');
 
 	console.log('Memvalidasi cookie');
-	const isInvalidCookies = page.url().includes('loginpage');
+	const isInvalidCookies = page.url().includes('login');
 
 	if (isInvalidCookies) {
 		console.log('Cookie tidak valid');
@@ -76,73 +94,130 @@ async function postFeed(browser: Browser, content: Content): Promise<void> {
 	}
 	console.log('Cookie valid');
 
+	console.log('Mengecek tipe konten');
+	if (content.TYPE === '') {
+		console.log('Tipe konten kosong');
+		await browser.deleteMatchingCookies(...cookies);
+		return;
+	}
+	if (content.ROUTE === 'BM') {
+		console.log(`Tipe konten ${content.TYPE.toLowerCase()}`);
+		fileSelector = 'text=Add photo/video';
+		captionSelector = 'text=Text';
+	}
+	if (content.ROUTE === 'PERSONAL') {
+		console.log(`Tipe konten ${content.TYPE.toLowerCase()}`);
+		fileSelector = 'text=Photo/video';
+		captionSelector = `xpath=//div[@role="button" and .//span[text()="What's on your mind?"]]`;
+	}
+
 	console.log('Mulai memposting konten');
 
-	console.log('Mencari tombol upload');
-	const fileSelector = 'text=Add photo/video';
-	const [fileTrigger] = await Promise.all([
-		page.waitForFileChooser().catch(() => null),
-		page
-			.locator(fileSelector)
-			.click()
-			.catch(() => null),
-	]);
+	if (content.TYPE !== 'POST') {
+		console.log('Mencari tombol upload');
+		const [fileTrigger] = await Promise.all([
+			page.waitForFileChooser().catch(() => null),
+			page
+				.locator(fileSelector)
+				.wait()
+				.catch(() => null),
+		]);
 
-	if (!fileTrigger) {
-		console.log('Tombol upload tidak ditemukan');
-		await browser.deleteMatchingCookies(...cookies);
-		return;
+		if (!fileTrigger) {
+			console.log('Tombol upload tidak ditemukan');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		console.log('Tombol upload ditemukan');
+
+		console.log('Mengupload asset(s)');
+		const isValidAsset = await assetChecker(content.PATH);
+		if (!isValidAsset) {
+			console.log('Asset(s) bermasalah');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		await fileTrigger.accept([content.PATH]);
+		console.log('Asset(s) diupload');
 	}
-	console.log('Tombol upload ditemukan');
 
-	console.log('Mengupload asset(s)');
-	const isValidAsset = await assetChecker(content.PATH);
-	if (!isValidAsset) {
-		console.log('Asset(s) bermasalah');
-		await browser.deleteMatchingCookies(...cookies);
-		return;
-	}
-	await fileTrigger.accept([content.PATH]);
-	console.log('Asset(s) diupload');
-
-	console.log('Menulis caption');
-	const captionSelector = 'text=Text';
 	const captionTrigger = page.locator(captionSelector);
 	await captionTrigger.click();
-	await page.keyboard.press('Tab');
-	await page.keyboard.type(content.CAPTION);
 
-	console.log('Mencari tombol publish');
-	const publishSelector = 'xpath=//div[@role="button" and .//div[text()="Publish"]]';
-	const publishTrigger = page.locator(publishSelector);
-
-	if (!publishTrigger) {
-		console.log('Tombol publish tidak ditemukan');
-		await browser.deleteMatchingCookies(...cookies);
-		return;
+	console.log('Menulis caption');
+	if (content.ROUTE === 'BM') {
+		await page.keyboard.press('Tab');
+		await page.keyboard.type(content.CAPTION + ' ');
+		await page.keyboard.press('Tab');
+	} else {
+		const createPostSelector = 'text=Add to your post';
+		await page.locator(createPostSelector).wait();
+		await page.keyboard.type(content.CAPTION + ' ');
+		await page.keyboard.press('Tab');
 	}
-	console.log('Tombol publish ditemukan');
 
-	console.log('Memvalidasi publish');
-	const isPublishValid = await isFacebookPublishButtonEnable(page, publishSelector);
+	if (content.ROUTE === 'BM') {
+		console.log('Mencari tombol publish');
+		const publishSelector = 'xpath=//div[@role="button" and .//div[text()="Publish"]]';
+		const publishTrigger = await page
+			.locator(publishSelector)
+			.waitHandle()
+			.catch(() => null);
 
-	if (!isPublishValid) {
-		console.log('Publish tidak valid');
-		await browser.deleteMatchingCookies(...cookies);
-		return;
-	}
-	await publishTrigger.click();
-	console.log('Publish valid');
+		if (!publishTrigger) {
+			console.log('Tombol publish tidak ditemukan');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		console.log('Tombol publish ditemukan');
 
-	console.log('Sedang memposting konten');
-	const postedSelector = 'text=Your post is published';
-	const postedTrigger = await page.locator(postedSelector).waitHandle();
-	const isPosted = await postedTrigger.isVisible().catch(() => null);
+		console.log('Memvalidasi publish');
+		const isPublishValid = await isFacebookPublishButtonEnable(page, publishSelector);
 
-	if (!isPosted) {
-		console.log('Gagal mempublish');
-		await browser.deleteMatchingCookies(...cookies);
-		return;
+		if (!isPublishValid) {
+			console.log('Publish tidak valid');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		await publishTrigger.click();
+		console.log('Publish valid');
+
+		const postedSelector = 'text=Your post is published';
+		await page.locator(postedSelector).wait();
+	} else {
+		console.log('Mencari tombol next');
+		const nextPostSelector = 'text=Next';
+		const nextPostTrigger = await page
+			.locator(nextPostSelector)
+			.waitHandle()
+			.catch(() => null);
+
+		if (!nextPostTrigger) {
+			console.log('Tombol next tidak ditemukan');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		await nextPostTrigger.click();
+		console.log('Tombol next ditemukan');
+
+		console.log('Memvalidasi publish');
+		const postPreviewSelector = 'text=Post preview';
+		const postPreviewTrigger = await page
+			.locator(postPreviewSelector)
+			.waitHandle()
+			.catch(() => null);
+
+		if (!postPreviewTrigger) {
+			console.log('Publish tidak valid');
+			await browser.deleteMatchingCookies(...cookies);
+			return;
+		}
+		await postPreviewTrigger.click();
+		await page.keyboard.down('Shift');
+		await page.keyboard.press('Tab');
+		await page.keyboard.up('Shift');
+		await page.keyboard.press('Enter');
+		console.log('Publish valid');
 	}
 
 	console.log('Selesai memposting konten');
