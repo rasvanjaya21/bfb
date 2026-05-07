19ccc79a0655064e93a86e5e859c5ff7c41ef30c
rasvanjaya21 <55085697+rasvanjaya21@users.noreply.github.com>
2026-05-07 18:41:14 +0700

    refactor(lib): migrate to new activation conn with hide input

--- src/libs/activate-bfb.ts
+++ src/libs/activate-bfb.ts
@@ -1,60 +1,37 @@
 import { applyDelay } from '@/libs/apply-delay';
-import crypto from 'crypto';
+import { hideQuestion } from '@/libs/hide-question';
 import fs from 'fs/promises';
 import path from 'path';
-import readline from 'readline/promises';
 
-async function activateBfb(readlineInterface: readline.Interface): Promise<void> {
+const BACKEND_URL_API = 'https://bfb.blackfriday.my.id/api/v1/check';
+
+async function activateBfb(): Promise<void> {
 	try {
-		const token = await readlineInterface.question('Masukkan token: ');
+		const token = await hideQuestion('Masukkan token: ');
 
 		if (!token) {
-			readlineInterface.pause();
 			console.clear();
 			console.log('Token kosong, aktifasi gagal\n');
 			await applyDelay(1000);
-			readlineInterface.resume();
 			return;
 		}
 
-		const res = await fetch('https://bfb.blackfriday.my.id/api/v1/check', {
-			method: 'GET',
+		const response = await fetch(BACKEND_URL_API, {
+			method: 'POST',
 			headers: {
-				Authorization: `Bearer ${token}`,
 				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
 			},
 		});
 
-		if (!res.ok) {
-			readlineInterface.pause();
-			console.clear();
-			console.log('Response error, aktifasi gagal\n');
-			await applyDelay(1000);
-			readlineInterface.resume();
-			return;
-		}
+		const data = (await response.json()) as { token: string };
 
-		const json = await res.json();
-
-		if (json?.state !== true) {
-			readlineInterface.pause();
+		if (!data.token) {
 			console.clear();
 			console.log('Token tidak valid, aktifasi gagal\n');
 			await applyDelay(1000);
-			readlineInterface.resume();
 			return;
 		}
-
-		const iv = crypto.randomBytes(16);
-
-		const key = crypto.createHash('sha256').update('belly-sedang-memancing').digest();
-
-		const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-
-		let encrypted = cipher.update(token);
-		encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-		const payload = `${iv.toString('base64')}:${encrypted.toString('base64')}`;
 
 		const cwd = process.cwd();
 		const dirPath = path.join(cwd, 'credentials');
@@ -62,20 +39,16 @@ async function activateBfb(readlineInterface: readline.Interface): Promise<void>
 
 		await fs.mkdir(dirPath, { recursive: true });
 
-		await fs.writeFile(filePath, payload, 'utf-8');
+		await fs.writeFile(filePath, data.token, 'utf-8');
 
-		readlineInterface.pause();
 		console.clear();
 		console.log('Token valid, aktifasi berhasil\n');
 		await applyDelay(1000);
-		readlineInterface.resume();
 		return;
 	} catch {
-		readlineInterface.pause();
 		console.clear();
 		console.log('Server error, aktifasi gagal\n');
 		await applyDelay(1000);
-		readlineInterface.resume();
 		return;
 	}
 }
